01ac06dc0ca881ff2f00db7f188f7ffd7749d840
bia-kono <bia-1994@live.com>
2025-03-21 18:09:37 -0300

    Primeiro Commit

---
+++ src/main/java/com/example/agendaComercioSA/AgendaComercioSaApplication.java
@@ -0,0 +1,13 @@
+package com.example.agendaComercioSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaComercioSaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaComercioSaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/agendaComercioSA/AgendaComercioSaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agendaComercioSA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaComercioSaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
